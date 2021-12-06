6774b44dfd88e9845de8ad5a6338d170689681b0
Roman Mahola <roman.mahola@gmail.com>
2021-12-06 11:14:27 +0200

    Facade

---
+++ app/Services/Parser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+class Parser
+{
+    public function getLinks()
+    {
+        return 'links';
+    }
+    public function getArticles()
+    {
+        return 'articles';
+    }
+    public function getPage()
+    {
+        return 'page';
+    }
+}
