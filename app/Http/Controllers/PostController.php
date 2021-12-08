c0c43f1dae6f155453dad385ac43319a405038b0
roman.mahola <roman.mahola@gmail.com>
2021-12-08 22:48:09 +0200

    Add route for Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(['message' => 'Ok'], Response::HTTP_OK);
+    }
 }
